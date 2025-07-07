b396817fd13ba9d224f193efc5ca4e44a544fde3
CormacSharkey <csharkey@tcd.ie>
2025-07-07 18:59:27 +0100

    tweaking the html to be centered and clean

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 import os
 from dotenv import load_dotenv
 
+from datetime import datetime
+
 from watchmode import Watchmode
 from smtp import Smtp
-
-import smtplib, ssl
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+from utils import type_map, type_check
 
 load_dotenv()
 
@@ -17,33 +16,35 @@ releases = watchmode.request_new_releases()
 html = """\
 <html>
 <body>
-<div style="display: table;">
-<div style="display: table-row;">
+<center>
+<div style="border: 8px solid red; display: table;">
 """
 
 column = 0
 
+cell_width = "230"
+
 for item in releases:
-    if column == 7:
+    if column == 6:
         column = 0
         html += """\
         </div>
-        <div style="display: table-row;">
+        <div style="border: 8px solid red; display: table;">
         """
 
     html += f"""\
-    <div style="width: 205px; display: table-cell;">
-    <img src={item.poster_url} width=200px style="border: 3px solid black;" alt="Poster Image">
-    <p style="border: 3px solid black;">
-    Title: {item.title}<br>
-    Type: {item.type}<br>
+    <div style="width: 245px; display: table-cell;">
+    <center>
+    <img src={item.poster_url} width={cell_width}px style="border: 3px solid black;" alt="Poster Image">
+    <p style="width: {cell_width}px; border: 3px solid black;">
+    <b>{item.title}</b><br>
+    Type: {type_map(item.type)}<br>
     Season Number: {item.season_number}<br>
     Release Date: {item.source_release_date}<br>
     Platform: {item.source_name}
     </p>
+    </center>
     </div>
-
-    <div style="width: 10px; display: table-cell;"></div>
 
     """
     column += 1
@@ -51,12 +52,13 @@ for item in releases:
 html += """\
 </div>
 </div>
+</center>
 </body>
 </html>
 """
 
 smtp = Smtp(os.getenv('APP_PASSWORD'), os.getenv('SENDER_EMAIL'), os.getenv('RECEIVER_EMAIL'))
 
-smtp.send_mail("smtp testing - borders", html)
+smtp.send_mail(f"html testing {datetime.now()}", html)
 
 # print(html)
---
+++ src/utils.py
@@ -0,0 +1,14 @@
+
+
+def type_map(type):
+    if type == "movie":
+        return "Movie"
+    elif type == "tv_series":
+        return "TV Series"
+    elif type == "tv_miniseries":
+        return "TV Miniseries"
+    
+def type_check(type):
+    if type == "movie":
+        return True
+    return False
