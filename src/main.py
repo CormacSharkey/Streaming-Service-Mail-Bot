8cf154a753c5a74425aa44d23ae1b7351781a7c2
CormacSharkey <csharkey@tcd.ie>
2025-07-07 14:47:21 +0100

    updated html to table structure and added borders

--- src/main.py
+++ src/main.py
@@ -17,29 +17,46 @@ releases = watchmode.request_new_releases()
 html = """\
 <html>
 <body>
+<div style="display: table;">
+<div style="display: table-row;">
 """
 
+column = 0
+
 for item in releases:
+    if column == 7:
+        column = 0
+        html += """\
+        </div>
+        <div style="display: table-row;">
+        """
+
     html += f"""\
-    <div>
-    <img src={item.poster_url} alt="Poster Image">
-    <p>
-    {item.title}<br>
-    {item.type}<br>
-    {item.season_number}<br>
-    {item.source_release_date}<br>
-    {item.source_name}
+    <div style="width: 205px; display: table-cell;">
+    <img src={item.poster_url} width=200px style="border: 3px solid black;" alt="Poster Image">
+    <p style="border: 3px solid black;">
+    Title: {item.title}<br>
+    Type: {item.type}<br>
+    Season Number: {item.season_number}<br>
+    Release Date: {item.source_release_date}<br>
+    Platform: {item.source_name}
     </p>
     </div>
+
+    <div style="width: 10px; display: table-cell;"></div>
+
     """
+    column += 1
 
 html += """\
+</div>
+</div>
 </body>
 </html>
 """
 
 smtp = Smtp(os.getenv('APP_PASSWORD'), os.getenv('SENDER_EMAIL'), os.getenv('RECEIVER_EMAIL'))
 
-smtp.send_mail("smtp panel testing", html)
+smtp.send_mail("smtp testing - borders", html)
 
 # print(html)
